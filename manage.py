53db3e8b69bc253f662bc2bec17751cd2d085847
José Filho <100032235+josecsfilho@users.noreply.github.com>
2023-01-03 22:14:31 -0300

    Add files via upload

---
+++ manage.py
@@ -0,0 +1,10 @@
+import pyautogui
+
+pyautogui.PAUSE = 2
+
+#  abrir ferramenta/ sistema
+pyautogui.press("win")
+pyautogui.write("google")
+pyautogui.press("enter")
+pyautogui.write("https://www.dio.me/")
+pyautogui.press("enter")
